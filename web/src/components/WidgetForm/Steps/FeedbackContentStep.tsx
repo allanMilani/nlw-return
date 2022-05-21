9d7e0e2c6db5f496717fc913f8085bf7082f7463
Allan Milani <allan.milani582@gmail.com>
2022-05-21 10:44:56 -0300

    aula 5

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "./Loading";
 import { ScreenShotButton } from "./ScreenShotButton";
 
 interface FeedbackContentStepProps {
@@ -17,13 +19,19 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<String | null>(null);
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({ screenshot, comment });
-
+    setIsSendingFeedback(true);
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -46,9 +54,10 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
         <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-400 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-node scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-400 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-node scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
           onChange={event => setComment(event.target.value)}
+          autoCorrect="false"
         />
         <footer className="flex gap-2 mt-2">
           <ScreenShotButton
@@ -57,10 +66,10 @@ export function FeedbackContentStep({
           />
           <button
             type="submit"
-            disabled={comment.length === 0}
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+            disabled={comment.length === 0 || isSendingFeedback}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
